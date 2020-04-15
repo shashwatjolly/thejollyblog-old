ef4e10c525a99218b3997c9a561bf0ce5e3e5443
Shashwat Jolly <shashwat.jolly@gmail.com>
2020-04-16 00:35:49 +0530

    Update blog posts

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,25 @@
 require(`dotenv`).config({
-  path: `.env`,
-})
+  path: `.env`
+});
 
 module.exports = {
   siteMetadata: {
-    siteTitleAlt: `Minimal Blog - Gatsby Theme`,
+    // Used for the title template on pages other than the index site
+    siteTitle: `thejollyblog`,
+    // Default title of the page
+    siteTitleAlt: `thejollyblog`,
+    // Can be used for e.g. JSONLD
+    siteHeadline: `Random thoughts and some useful stuff (maybe) by an absolute doofus`,
+    // Will be used to generate absolute URLs for og:image etc.
+    siteUrl: `https://thejollyblog.netlify.com`,
+    // Used for SEO
+    siteDescription: `Shashwat Jolly's blog`,
+    // Will be set on the <html /> tag
+    siteLanguage: `en`,
+    // Used for og:image and must be placed inside the `static` folder
+    siteImage: `/banner.jpg`,
+    // Twitter Handle
+    author: `@jollyshashwat`
   },
   plugins: [
     {
@@ -13,30 +28,38 @@ module.exports = {
         navigation: [
           {
             title: `Blog`,
-            slug: `/blog`,
+            slug: `/blog`
           },
           {
             title: `About`,
-            slug: `/about`,
-          },
+            slug: `/about`
+          }
         ],
         externalLinks: [
           {
             name: `Twitter`,
-            url: `https://twitter.com/lekoarts_de`,
+            url: `https://twitter.com/jollyshashwat`
           },
           {
             name: `Instagram`,
-            url: `https://www.instagram.com/lekoarts.de/`,
+            url: `https://www.instagram.com/thejollyguy/`
           },
-        ],
-      },
+          {
+            name: `Facebook`,
+            url: `https://www.facebook.com/shashwatjolly`
+          },
+          {
+            name: `Github`,
+            url: `https://www.github.com/shashwatjolly`
+          }
+        ]
+      }
     },
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: process.env.GOOGLE_ANALYTICS_ID,
-      },
+        trackingId: process.env.GOOGLE_ANALYTICS_ID
+      }
     },
     `gatsby-plugin-sitemap`,
     {
@@ -53,18 +76,18 @@ module.exports = {
           {
             src: `/android-chrome-192x192.png`,
             sizes: `192x192`,
-            type: `image/png`,
+            type: `image/png`
           },
           {
             src: `/android-chrome-512x512.png`,
             sizes: `512x512`,
-            type: `image/png`,
-          },
-        ],
-      },
+            type: `image/png`
+          }
+        ]
+      }
     },
     `gatsby-plugin-offline`,
-    `gatsby-plugin-netlify`,
+    `gatsby-plugin-netlify`
     // `gatsby-plugin-webpack-bundle-analyser-v2`,
-  ],
-}
+  ]
+};
